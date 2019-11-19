a71f0f2a66688256dd8aaa378e7ed79151cbf89b
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2019-11-19 23:14:25 +0900

    a

--- js/common.js
+++ js/common.js
@@ -75,6 +75,7 @@ $(function(){
     mounted () {
       fetch(
         'https://script.google.com/macros/s/AKfycbykP5rFHcjf_Sd-u0u5_iRoqUlHNl_A02IyjsECYOeaO_Vn00Ap/exec',
+        //"https://script.google.com/macros/s/AKfycbzSdN3_6l87Bbn58AFWgq7lFnI27blOi7jWn0JdWYVRaGZWwOSd/exec",
       )
         .then(res => res.json())
         .then(result => {
