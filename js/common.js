9c22fd6335e4ff1dcea2a8a95d45707c01c6407c
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2019-12-16 23:57:10 +0900

    a

--- js/common.js
+++ js/common.js
@@ -74,7 +74,7 @@ $(function(){
     el: '#app',
     mounted () {
       fetch(
-        /*
+        //*
         //*
         'https://script.google.com/macros/s/AKfycbykP5rFHcjf_Sd-u0u5_iRoqUlHNl_A02IyjsECYOeaO_Vn00Ap/exec',
         /*/
